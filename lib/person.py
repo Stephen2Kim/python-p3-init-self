a1f1bf4dd43889a904619fa324cd05a398c199b4
Stephen Muniu Kimani <stvnkimani@gmail.com>
2024-10-14 23:43:04 +0300

    Implement Person class with constructor to initialize name attribute.

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        """Initializes a Person instance with a name.
+
+        Args:
+            name (str): The name of the person.
+        """
+        self.name = name
